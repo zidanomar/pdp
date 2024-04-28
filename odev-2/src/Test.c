b2c88adc3033bff223ae77f42289d39e7f6001a4
Zidan Omar Hamid <zidanomar@430am.dev>
2024-04-28 17:32:53 +0300

    fixed tiles type always replacing species value

--- odev-2/src/Test.c
+++ odev-2/src/Test.c
@@ -1,99 +1,99 @@
-#include "Habitat.h"
-#include "Bitki.h"
-int main()
-{
-    Bitki b = NewBitki(1, "Bitki");
-    b->PrintHello();
-    b->DeleteBitki(b);
-    // Habitat h = NewHabitat();
-    // h->PrintHabitat(h);
-    // h->DeleteHabitat(h);
-    return 0;
+// #include "Habitat.h"
+// int main()
+// {
+//     Habitat h = NewHabitat();
+//     h->PrintHabitat(h);
+//     // h->DeleteHabitat(h);
+//     return 0;
 
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the struct for type 1
+struct INSECT {
+    int health;
+};
+typedef struct INSECT *Insect;
+
+Insect NewInsect(int health) {
+    Insect this;
+    this = (Insect)malloc(sizeof(struct INSECT));
+    this->health = health;
+    return this;
 }
 
-// #include <stdio.h>
+// Define the struct for type 2
+struct PLANT {
+    char *name;
+    int leaf;
+};
 
-// // Define the struct for type 1
-// struct Type1 {
-//     int value1;
-// };
+// Define the union struct
+union UnionStruct {
+    Insect insect;
+    struct PLANT plant;
+};
 
-// // Define the struct for type 2
-// struct Type2 {
-//     float value2;
-// };
+// Define enum to indicate type
+enum Type {
+    TYPE_INSECT,
+    TYPE_PLANT
+};
 
-// // Define the union struct
-// union UnionStruct {
-//     struct Type1 type1;
-//     struct Type2 type2;
-//     char flag;
-// };
+struct Tile {
+    enum Type type;
+    union UnionStruct data;
+};
 
-// int main() {
-//     int rows, cols = 5;
-//     // create a 2D array of union structs with malloc
-//     union UnionStruct **tiles;
-//     tiles = (union UnionStruct **)malloc(rows * sizeof(union UnionStruct*));
-//     if (tiles == NULL) {
-//         free(tiles);
-//         return NULL;
-//     }
-//     for (int i = 0; i < rows; ++i)
-//     {
-//         tiles[i] = (union UnionStruct *)malloc(cols * sizeof(union UnionStruct*));
-//         if (tiles[i] == NULL) {
-//             for (int j = 0; j < i; j++) {
-//                 free(tiles[j]);
-//             }
-//             free(tiles);
-//             free(tiles);
-//             return NULL;
-//         }
-//     }
+int main() {
+    int row = 5;
+    int col = 4;
 
-//     // Initialize the union structs with values and flags
-//     for (int i = 0; i < rows; ++i)
-//     {
-//         for (int j = 0; j < cols; ++j)
-//         {
-//             if (i % 2 == 0) {
-//                 tiles[i][j].type1.value1 = 10;
-//                 tiles[i][j].flag = 'A';
-//             }
-//             else {
-//                 tiles[i][j].type2.value2 = 3.14;
-//                 tiles[i][j].flag = 'B';
-//             }
-//         }
-//     }
+    struct Tile **tiles = (struct Tile **)malloc(row * sizeof(struct Tile *));
+    for (int i = 0; i < row; ++i) {
+        tiles[i] = (struct Tile *)malloc(col * sizeof(struct Tile));
+    }
 
-//     // Print different output based on the flag inside the union struct
-//     for (int i = 0; i < rows; i++) {
-//         for (int j = 0; j < cols; j++) {
-//             printf("Flag: %c\n", tiles[i][j].flag);
-//             switch (tiles[i][j].flag)
-//             {
-//             case 'A':
-//                 printf("Value a: %d\n", tiles[i][j].type1.value1);
-//                 break;
-//             case 'B':
-//                 printf("Value b: %f\n", tiles[i][j].type2.value2);
-//                 break;
-//             default:
-//                 break;
-//             }
-//         }
-//     }
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (j % 2 == 0)
+            {
+                tiles[i][j].type = TYPE_INSECT;
+                tiles[i][j].data.insect = NewInsect(100);
+            } else {
+                tiles[i][j].type = TYPE_PLANT;
+                tiles[i][j].data.plant.name = "Rose";
+                tiles[i][j].data.plant.leaf = 10;
+            }
+            
+        }
+    }
+    
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (tiles[i][j].type == TYPE_INSECT)
+            {
+                printf("Insect health: %d\n", tiles[i][j].data.insect->health);
+            } else {
+                printf("Plant name: %s, leaf: %d\n", tiles[i][j].data.plant.name, tiles[i][j].data.plant.leaf);
+            }
+        }
+    }
 
-//     // clear the memory
-//     for (int i = 0; i < rows; ++i)
-//     {
-//         free(tiles[i]);
-//     }
+    for (int i = 0; i < row; ++i) {
+        free(tiles[i]);
+    }
 
-//     free(tiles);
+    free(tiles);
 
-//     return 0;
-// }
+    return 0;
+}
